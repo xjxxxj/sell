c1dd056df80dfd123eef82dbbf5d859f4458bb00
xjxxxj <1690091317@qq.com>
2018-11-16 10:46:16 +0800

    sell项目包结构的搭建以及通过成功启动通过Hello World测试

---
+++ src/main/java/com/xinyan/sell/dto/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.xinyan.sell.dto;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: sell
+ * @description: 测试项目能否正常启动
+ * @author: 谢庆香
+ * @create: 2018-11-16 09:56
+ **/
+
+public class HelloWorld {
+
+    public String hello(){
+        return "Hello World!" ;
+    }
+
+}
