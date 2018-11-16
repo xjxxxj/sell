96272f80c2c03d7c54286344425f63fc921a4c71
lxj19900406 <lxj08055122@163.com>
2018-11-16 17:01:32 +0800

    封装订单人信息

--- src/main/java/com/xinyan/sell/vo/Product.java
+++ src/main/java/com/xinyan/sell/vo/Product.java
@@ -2,6 +2,9 @@ package com.xinyan.sell.vo;
 
 import java.io.Serializable;
 
+/**
+ * 封装product商品ID和商品数量
+ */
 public class Product implements Serializable {
     private static final long serialVersionUID = 423939619062109326L;
     //商品id
@@ -9,23 +12,5 @@ public class Product implements Serializable {
     //商品数量
     private Integer productQuantity;
 
-    public static long getSerialVersionUID() {
-        return serialVersionUID;
-    }
 
-    public Integer getProductId() {
-        return productId;
-    }
-
-    public void setProductId(Integer productId) {
-        this.productId = productId;
-    }
-
-    public Integer getProductQuantity() {
-        return productQuantity;
-    }
-
-    public void setProductQuantity(Integer productQuantity) {
-        this.productQuantity = productQuantity;
-    }
 }
