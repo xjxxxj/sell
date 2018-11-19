d4a89030ae7d803e57bfadd62c07697107fd181e
lxj19900406 <lxj08055122@163.com>
2018-11-19 17:26:44 +0800

    订单接口类

--- src/main/java/com/xinyan/sell/service/OrderService.java
+++ src/main/java/com/xinyan/sell/service/OrderService.java
@@ -1,5 +1,8 @@
 package com.xinyan.sell.service;
 
+import com.xinyan.sell.po.Order;
+import org.springframework.data.domain.Page;
+
 /**
  * @Description: 订单服务接口
  * @Author: 谢庆香
@@ -8,4 +11,39 @@ package com.xinyan.sell.service;
  */
 
 public interface OrderService {
+    /*@program：订单（order）
+     *创建订单
+    *@author 李显君
+    *@date 2018/11/19 16:42
+    */
+     Order createOrder(Order order);
+    
+    /*@program：订单（order）
+     *订单列表
+    *@author 李显君
+    *@date 2018/11/19 16:53
+    */
+     Page<Order> findList(Order order);
+    /*@program：订单（order）
+    *查询订单
+    *@author 李显君
+    *@date 2018/11/19 17:16
+    */
+     Order findOrderById(String orderId);
+    
+    /*@program：订单（order）
+    *取消订单
+    *@author 李显君
+    *@date 2018/11/19 17:19
+    */
+     Order cancelOrder(Order order);
+    
+    
+    /*@program：订单（order）
+    *支付订单
+    *@author 李显君
+    *@date 2018/11/19 17:21
+    */
+     Order payOrder(Order order);
+
 }
